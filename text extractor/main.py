b5043dccc6ed84131d6c0218ffc5fa2c6ae39bdd
n <nicksonthehuman@gmail.com>
2025-05-30 15:44:36 +0800

    now it can read from multiple files and it stores the output to a csv file

--- text extractor/main.py
+++ text extractor/main.py
@@ -1,10 +1,21 @@
+import os
 import pymupdf
+import pandas as pd
 
-doc = pymupdf.open(r"text extractor\documents\test.docx")
-out = open(r"text extractor\output.txt", "wb")
-print(doc.page_count)
-for page in doc: # iterate the document pages
-    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
-    out.write(text) # write text of page
-    out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
-out.close()
+df = pd.DataFrame(
+    {
+        "text": [], 
+    }
+)
+
+docs_path = r"text extractor\documents"
+file_names = [os.path.join(docs_path, f) for f in os.listdir(docs_path) if os.path.isfile(os.path.join(docs_path, f))]
+
+for file_name in file_names:
+    doc = pymupdf.open(file_name)
+    text = ""
+    for page in doc: # iterate the document pages
+        text = text + page.get_text() + "\f"
+    df.loc[len(df)] = [text]
+
+df.to_csv(r"text extractor\output.csv")
